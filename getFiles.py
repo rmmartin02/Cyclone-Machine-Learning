e9bd7e46a6383710e8eeca96f6fe0dd5d144c50f
Russell Martin <rmmartin02@email.wm.edu>
2018-07-16 23:19:38 -0400

    added file to download images, could maybe multithread them?(ie spawn a thread for every download)

--- getFiles.py
+++ getFiles.py
@@ -2,38 +2,74 @@ import urllib.request
 import requests
 import os
 from bs4 import BeautifulSoup
+import datetime
 
-#reconArchive = 'https://www.nhc.noaa.gov/archive/recon/{}/{}/{}.{}.txt'.format(year,dataType,dataType,date)
+def saveTextFiles(year,reconArchive,dataType,plane=None,):
+    print(reconArchive)
+    page = requests.get(reconArchive)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-#year satellite archive starts
+    allAs = soup.find_all('a', href=True)
+    txtFiles = []
+    for a in allAs:
+        if '.txt' in str(a):
+            txtFiles.append(str(a).split('"')[1])
+
+    for t in txtFiles:
+        directory = './{}/{}/'.format(year, dataType)
+        if year>2006 and year < 2012:
+            directory = './{}/{}/{}/'.format(year, plane, dataType)
+        if year>2011:
+            directory = './{}/{}/'.format(year, dataType)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_name = directory + t
+        if not os.path.isfile(file_name):
+            url = reconArchive + t
+            print('Downloading: {}'.format(t))
+            urllib.request.urlretrieve(url, file_name)
+        else:
+            print('Already have: {}'.format(t))
+
+# reconArchive = 'https://www.nhc.noaa.gov/archive/recon/{}/{}/{}.{}.txt'.format(year,dataType,dataType,date)
+
+# year satellite archive starts
 year = 2006
-while True:
-	#observation names change, also adds western pacific
-	dataType2011 = ['HDOB','REPNT2','REPNT3','URNT40']
-	dataType2012 = ['AHONT1','REPNT2','REPNT3','AHOPN1','REPPN2','REPPN3','AHOPA1','REPPA2','REPPA3']
-	reconArchive = 'https://www.nhc.noaa.gov/archive/recon/{}/{}/'.format(year,dataType)
-	print(reconArchive)
+while year<=datetime.date.today().year:
+    # this only works for 2006-2011
+    # observation names change, also adds western pacific
+    dataTypes2006 = ['HDOB']
+    # for HDOB 2007-2011
+    orgs = ['NOAA', 'USAF']
+    planes = ['URNT15', 'URPN15']
 
-	page = requests.get(reconArchive)
-	soup = BeautifulSoup(page.text,'html.parser')
+    # 2012
+    dataTypes2012 = ['AHONT1', 'AHOPA1', 'AHOPN1']
+    # 2016-2017 has no AHOPA1
+    reconArchive = 'https://www.nhc.noaa.gov/archive/recon/'+str(year)+'/'
+    if year<=2011:
+        for dataType in dataTypes2006:
+            reconArchive = reconArchive + dataType + '/'
+            if year!=2006:
+                for org in orgs:
+                    reconArchive = reconArchive + org + '/'
+                    if year!=2007:
+                        for plane in planes:
+                            reconArchive = reconArchive + plane + '/'
+                            saveTextFiles(year, reconArchive, dataType, plane=plane)
+                            reconArchive = 'https://www.nhc.noaa.gov/archive/recon/' + str(year) + '/'
+                            reconArchive = reconArchive + dataType + '/'
+                            reconArchive = reconArchive + org + '/'
+                    else:
+                        saveTextFiles(year, reconArchive, dataType)
+                        reconArchive = 'https://www.nhc.noaa.gov/archive/recon/' + str(year) + '/'
+                        reconArchive = reconArchive + dataType + '/'
+            else:
+                saveTextFiles(year, reconArchive, dataType)
+    else:
+        for dataType in dataTypes2012:
+            '{}{}/'.format(reconArchive,dataType)
+            saveTextFiles(year, reconArchive, dataType)
 
-	allAs = soup.find_all('a',href=True)
-	txtFiles = []
-	for a in allAs:
-		if '.txt' in str(a):
-			txtFiles.append(str(a).split('"')[1])
+    year += 1
 
-	if len(txtFiles)==0:
-		break
-
-	for t in txtFiles:
-		if not os.path.exists('./{}/{}/'.format(year,dataType)):
-			os.makedirs('./{}/{}/'.format(year,dataType))
-		file_name = './{}/{}/{}'.format(year,dataType,t)
-		if not os.path.isfile(file_name):
-			url = reconArchive + t
-			print('Downloading: {}'.format(t))
-			urllib.request.urlretrieve(url, file_name)
-		else:
-			print('Already have: {}'.format(t))
-	year += 1
---
+++ getImages.py
@@ -0,0 +1,47 @@
+import urllib.request
+import requests
+import os
+from bs4 import BeautifulSoup
+import datetime
+
+baseURL = 'http://rammb.cira.colostate.edu/'
+stormsURL = baseURL + 'products/tc_realtime/products/storms/'
+
+page = requests.get(stormsURL)
+soup = BeautifulSoup(page.text, 'html.parser')
+allAs = soup.find_all('a', href=True)
+
+stormDIRs = []
+for a in allAs:
+    if 'storms' in str(a):
+        stormDIRs.append(str(a).split('"')[1].split('/')[5])
+
+for storm in stormDIRs:
+    stormURL = '{}{}/'.format(stormsURL,storm)
+    #get 4KMIRIMG
+    products = ['4KMIRIMG']
+    for product in products:
+        productURL = '{}{}'.format(stormURL,product)
+        print(productURL)
+        page = requests.get(productURL)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        allAs = soup.find_all('a', href=True)
+
+        images = []
+        for a in allAs:
+            if '.GIF' in str(a):
+                images.append(str(a).split('"')[1].split('/')[7])
+
+        for image in images:
+            directory = './images/{}/{}/'.format(storm,product)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            file_name = directory + image
+            if not os.path.isfile(file_name):
+                url = '{}/{}'.format(productURL,image)
+                print('Downloading: {}'.format(image))
+                urllib.request.urlretrieve(url, file_name)
+            else:
+                print('Already have: {}'.format(image))
+
+#print(stormDIRs)
