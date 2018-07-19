fc8e00616998c01be989d396ba9b3a632d6dcebe
makoa <rmmartin02@email.wm.edu>
2018-07-19 17:48:21 -0400

    Made verify multithreaded

--- getScripts/verifyImages.py
+++ getScripts/verifyImages.py
@@ -1,8 +1,10 @@
 import os
 import urllib.request
 from PIL import Image
+from threading import Thread
+from queue import Queue
 
-for storm in os.listdir('./images/'):
+def verify(storm):
 	directory = './data/{}/STRMSTAT/'.format(storm)
 	if os.path.exists(directory):
 		stats = os.listdir(directory)
@@ -17,3 +19,23 @@ for storm in os.listdir('./images/'):
 					print('Error: {}'.format(image))
 					url = 'http://rammb.cira.colostate.edu/products/tc_realtime/products/storms/{}/4KMIRIMG/{}'.format(storm,image)
 					urllib.request.urlretrieve(url, directory+'/'+image)
+ 
+def do_work(q):
+    while True:
+        items = q.get()
+        func = items[0]
+        args = items[1:]
+        func(*args)
+        q.task_done()
+
+q = Queue(maxsize=0)
+num_threads = 64
+for i in range(num_threads):
+    worker = Thread(target=do_work,args=(q,))
+    worker.setDaemon(True)
+    worker.start()   
+
+for storm in os.listdir('./images/'):
+	q.put( (verify,storm))
+	
+q.join()
