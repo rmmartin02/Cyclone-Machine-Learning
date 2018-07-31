afb9bc1d92a46e46a39050a1dfe5fbdf91edb9eb
makoa <rmmartin02@email.wm.edu>
2018-07-31 17:01:36 -0400

    added more efficient script for scraping TCDAT image info

--- getScripts/exploreTCDat.py
+++ getScripts/exploreTCDat.py
@@ -5,11 +5,12 @@ base_url = 'https://www.nrlmry.navy.mil/tcdat/'
 banned = ['?C=N;O=D','?C=M;O=A','?C=S;O=A','?C=D;O=A','tcdat']
 allowed = []
 basins = ['ATL','CPAC','EPAC','IO','SHEM','WPAC']
-seasons = ['tc97','tc98','tc99','tc00','tc00','tc01','tc02','tc03','tc04','tc05','tc06','tc07','tc08','tc09','tc10','tc11','tc12','tc13','tc14','tc15','tc16','tc17','tc18','tc19']
-#seasons = ['tc97']
+#seasons = ['tc97','tc98','tc99','tc00','tc00','tc01','tc02','tc03','tc04','tc05','tc06','tc07','tc08','tc09','tc10','tc11','tc12','tc13','tc14','tc15','tc16','tc17','tc18','tc19']
+seasons = ['tc97']
 for season in seasons:
     allowed.append(base_url+season+'/')
 visited = []
+towrite = []
 
 def checkUrl(url):
     for a in allowed:
@@ -34,10 +35,25 @@ class TCDATSpider(Spider):
                         visited.append(url)
                         #print("url\t",url)
                         yield scrapy.Request(url, callback=self.parse)
+        with open('./getScripts/TCDAT.csv','a') as file:
+            file.writelines(towrite)
+        del towrite[:]
 
     def getSize(self,response):
         size = int(response.headers[b'Content-Length'])
         print('image\t' + response.url + '\tsize\t' + str(size))
-        parsed = ','.join('.'.join(response.url.split('/')[4:]).split('.')) + ',' + str(size)
-        with open('./getScripts/TCDAT.csv','a') as file:
-            file.write(parsed+'\n')
+        r = response.url.split('/')
+        path = r[4:-1]
+        image = r[-1]
+        if len(path)<6:
+            while len(path)<6:
+                path.append('')
+        image = image.split('.')
+        form = image[-1]
+        image = image[:-1]
+        if len(image)<9:
+            while len(image)<9:
+                image.append('')
+            image.append(form)
+        parsed = '{},{},{}\n'.format(','.join(path+image),size,response.url)
+        towrite.append(parsed)
---
+++ getScripts/getTCDatSpider.py
@@ -0,0 +1,62 @@
+import scrapy
+from scrapy.spiders import Spider
+
+base_url = 'https://www.nrlmry.navy.mil/tcdat/'
+banned = ['?C=N;O=D','?C=M;O=A','?C=S;O=A','?C=D;O=A','tcdat']
+allowed = []
+basins = ['ATL','CPAC','EPAC','IO','SHEM','WPAC']
+#seasons = ['tc97','tc98','tc99','tc00','tc00','tc01','tc02','tc03','tc04','tc05','tc06','tc07','tc08','tc09','tc10','tc11','tc12','tc13','tc14','tc15','tc16','tc17','tc18','tc19']
+seasons = ['tc97']
+for season in seasons:
+    allowed.append(base_url+season+'/')
+visited = []
+towrite = []
+
+def checkUrl(url):
+    for a in allowed:
+        if a in url:
+            return True
+    return False
+
+class TCDAT2Spider(Spider):
+    name = 'Explore TCDAT2'
+
+    start_urls = allowed
+
+    def parse(self, response):
+        urls = response.css("a::attr('href')").extract()
+        isImageDir = False
+        for url in urls:
+            if url not in banned:
+                url = response.urljoin(url)
+                if checkUrl(url):
+                    if '.jpg' in url or '.png' in url:
+                        #print('image\t',url)
+                        isImageDir = True
+                        break
+                    elif url not in visited and url[-1]=='/':
+                        visited.append(url)
+                        #print("url\t",url)
+                        yield scrapy.Request(url, callback=self.parse)
+        if isImageDir:
+            for line in response.text.split('\n'):
+                if '.jpg' in line or '.png' in line:
+                    a = line.split()
+                    if len(a)>4:
+                        size = a[-1][:-1]
+                        path = response.url.split('/')[4:-1]
+                        image = a[4].split('"')[1].split('.')
+                        if len(path)<6:
+                            while len(path)<6:
+                                path.append('')
+                        form = image[-1]
+                        image = image[:-1]
+                        if len(image)<9:
+                            while len(image)<9:
+                                image.append('')
+                            image.append(form)
+                        parsed = '{},{},{}\n'.format(','.join(path+image),size,response.url)
+                        towrite.append(parsed)
+            with open('./getScripts/TCDAT.csv','a') as file:
+                file.writelines(towrite)
+            del towrite[:]
